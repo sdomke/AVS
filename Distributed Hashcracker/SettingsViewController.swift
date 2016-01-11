61d238dc9865965bf991feff80d7e9e2e2ebf39a
Multitouch <multitouch@multitouchs-mac-mini.local>
2016-01-11 17:13:39 +0100

    Aktuelle Version

--- Distributed Hashcracker/SettingsViewController.swift
+++ Distributed Hashcracker/SettingsViewController.swift
@@ -24,6 +24,7 @@ class SettingsViewController: NSViewController {
         } else {
             serverAdressField.stringValue = ""
             serverAdressField.enabled = true
+        
         }
         
         // TODO: Setup Server
